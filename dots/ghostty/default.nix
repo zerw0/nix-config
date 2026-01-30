0ec108b2b2af4dd17b6dc1a389c664acda5eb779
Hristo Djenkov <me@zerw.xyz>
2026-01-30 11:11:06 +0200

    GhosTTY setup

--- dots/ghostty/default.nix
+++ dots/ghostty/default.nix
@@ -2,12 +2,15 @@
 {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty-bin;
+    package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
+    enableFishIntegration = true;
     settings = {
       theme = "Gruvbox Dark";
       font-size = 16;
       font-family = "CaskaydiaCove Nerd Font";
       adjust-cell-height = "20%";
+      background-opacity = "0.95";
+      shell-integration-features = [ "ssh-terminfo" ];
       font-thicken = true;
       font-thicken-strength = 120;
     };

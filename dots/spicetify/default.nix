e798126d12cfd81af6b67bd8881031eb55ff44d8
Hristo Djenkov <me@zerw.xyz>
2026-02-07 03:31:18 +0200

    asd

---
+++ dots/spicetify/default.nix
@@ -0,0 +1,16 @@
+{
+    pkgs,
+    lib,
+    inputs,
+    ...
+}:
+{
+  programs.spicetify =
+  let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  in
+  {
+    enable = true;
+    theme = spicePkgs.themes.onepunch;
+  };
+}

7a5152e610ea0f894263f4ce18a7801d5031fbc2
Hristo Djenkov <me@zerw.xyz>
2026-02-09 17:10:18 +0200

    disko

--- machines/nixos/htpc/disk-config.nix
+++ machines/nixos/htpc/disk-config.nix
@@ -11,37 +11,56 @@
               priority = 1;
               name = "ESP";
               start = "1M";
-              end = "128M";
+              end = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+                mountOptions = ["umask=0077"];
               };
             };
             root = {
               size = "100%";
               content = {
                 type = "btrfs";
-                extraArgs = [ "-f" ];
+                extraArgs = ["-f"]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
                 subvolumes = {
-                  "/rootfs" = {
+                  # Subvolume name is different from mountpoint
+                  "/root" = {
                     mountpoint = "/";
+                    mountOptions = ["subvol=root" "compress=zstd" "noatime"];
                   };
+                  # Subvolume name is the same as the mountpoint
                   "/home" = {
-                    mountOptions = [ "compress=zstd" ];
+                    mountOptions = ["subvol=home" "compress=zstd" "noatime"];
                     mountpoint = "/home";
                   };
+                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  "/home/user" = {};
+                  # Parent is not mounted so the mountpoint must be set
                   "/nix" = {
                     mountOptions = [
+                      "subvol=nix"
                       "compress=zstd"
                       "noatime"
                     ];
                     mountpoint = "/nix";
                   };
+                  "/log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                  };
+                  "/lib" = {
+                    mountpoint = "/var/lib";
+                    mountOptions = ["subvol=lib" "compress=zstd" "noatime"];
+                  };
+                  # This subvolume will be created but not mounted
+                  "/test" = {};
                 };
-                mountpoint = "/partition-root";
+
               };
             };
           };
@@ -49,4 +68,6 @@
       };
     };
   };
+  fileSystems."/var/log".neededForBoot = true;
+  fileSystems."/var/lib".neededForBoot = true;
 }

0e28b3c601bd946def12e3c8e6384bb9342defac
Hristo Djenkov <me@zerw.xyz>
2026-05-03 21:01:33 +0300

    cec support

--- machines/nixos/htpc/configuration.nix
+++ machines/nixos/htpc/configuration.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  myKodi = pkgs.kodi-gbm.withPackages (p: with p; [ jellycon peripheral-joystick ]);
+  myKodi = pkgs.kodi-gbm.withPackages (p: [ p.jellycon p."peripheral-joystick" ]);
 in
 {
   imports = [
